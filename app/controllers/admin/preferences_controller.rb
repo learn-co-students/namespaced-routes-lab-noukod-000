0621c022abc1d4ca9a5b0bdcf2a9a6ee1d66b42e
Gilbert Torchon <gilbert@noukod.com>
2019-01-30 15:11:26 +0000

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,5 @@
+class Admin::PreferencesController < ApplicationController
+  def index
+    #@preference=Preference.create(artist_sort_order:"DESC",song_sort_order:"ASC",allow_create_songs:true,allow_create_artists:true)
+  end
+end
