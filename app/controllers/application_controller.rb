a9dc5396c4546116f44bd99bf1751b668d935521
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-08-01 14:00:39 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
+
+require 'rack-flash'
+# require 'rails/redirect_with_flash'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
