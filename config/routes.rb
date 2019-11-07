e70c2aa6e32e8eb71b95296118efd34c3bb6135b
Andy Feidje Joseph <ingandy75@gmail.com>
2019-11-07 02:55:25 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
 
+  scope '/admin', module: 'admin' do
+    resources :preferences, only: [:index]
+  end
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
   resources :songs
 
+  namespace :admin do
+    resources :preferences, only: [:index, :update]
+  end
 end
